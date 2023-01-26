64cd88425c77d1eac19e34996558fda89112b8df
g00409592 <g00409592@atu.ie>
2023-01-26 12:25:58 +0000

    USed an ArrayList<Integer> to save values.

--- src/main/java/ie/atu/week1/ArrayDemo.java
+++ src/main/java/ie/atu/week1/ArrayDemo.java
@@ -2,19 +2,16 @@ package ie.atu.week1;
 
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ArrayDemo {
     public static void main(String[] args) {
-        int[] myElementArray = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
-
-        /*
-        for (int i = 0; i < myElementArray.length; i++)
+        ArrayList<Integer> myElementArray = new ArrayList<>();
+        for(int i = 1; i < 11; i++)
         {
-            System.out.println("Element at index " + i + ": " + myElementArray[i]);
+            myElementArray.add(100 * i);
         }
-
-         */
 
         int indexValue = 0;
 
@@ -26,23 +23,28 @@ public class ArrayDemo {
 
         Scanner myScanner = new Scanner(System.in);
 
-
+/*
         try
         {
             System.out.println("Please enter an integer number: ");
-            myElementArray[10] = myScanner.nextInt();
+            myElementArray.add(10, myScanner.nextInt());
         }
-        catch (ArrayIndexOutOfBoundsException e)
+        catch (InputMismatchException | IndexOutOfBoundsException e)
         {
-            System.out.println("Cannot proceed. The index is out of bounds.");
+            System.out.println("Cannot proceed. There was an error with your input.");
             System.out.println(e);
         }
+
+ */
+        System.out.println("Please enter an integer number: ");
+        myElementArray.add(10, myScanner.nextInt());
+        System.out.println("The new element added through user input is:" + myElementArray.get(10));
 
         System.out.println("\n\nThe program ended.");
     }
 
-    //public static void loop(ArrayList<Integer>)
+    public static void loop(ArrayList<Integer> localArray)
     {
-
+        System.out.println("temp print");
     }
 }
